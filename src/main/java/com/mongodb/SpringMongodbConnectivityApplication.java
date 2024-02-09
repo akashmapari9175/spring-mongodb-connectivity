353502dd9e3a6803751df6da2271572b93eba42e
akashmapari <akashmapari85@gmail.com>
2024-02-09 21:52:28 +0530

    MongoDb connectivity with spring

---
+++ src/main/java/com/mongodb/SpringMongodbConnectivityApplication.java
@@ -0,0 +1,13 @@
+package com.mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongodbConnectivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongodbConnectivityApplication.class, args);
+	}
+
+}
